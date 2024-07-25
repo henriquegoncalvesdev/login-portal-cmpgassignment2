e67b95893c71410c63d5384855b51d4dbb1af6ad
henrigon28 <henriquegoncalvesdev@gmail.com>
2024-07-25 14:49:20 -0400

    minor fix

--- inc/database.php
+++ inc/database.php
@@ -1,6 +1,6 @@
 <?php
 	try{
-		$conn = new PDO('mysql:host=localhost;dbname=mydb','root','root');
+		$conn = new PDO('mysql:host=localhost;dbname=mydb','root','');
 		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(PDOException $e){
